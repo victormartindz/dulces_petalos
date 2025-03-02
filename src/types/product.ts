b23b04c0afa9bd9e9fbd76e49b06bae6746d6014
victormartindz <victor.martin.dz@gmail.com>
2025-03-02 17:13:27 +0100

    added Product type

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  name: string;
+  scientificName: string;
+  price: number;
+  imageUrl: string;
+  waterPerWeek: number;
+  fertilizer: string;
+  height: number;
+}
